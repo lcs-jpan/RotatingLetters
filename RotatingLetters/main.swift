b74624c4f91a49c3bda7e9265e7d874ed8e4b818
Pan Jason <jpan@lcs-mba3130.local>
2018-05-22 15:02:22 -0400

    almost done

--- RotatingLetters/main.swift
+++ RotatingLetters/main.swift
@@ -11,6 +11,7 @@ import Foundation
 // INPUT
 
 // Loop forever untill we get good input
+var validInput = ""
 while 1 == 1 {
     
     // Prompt the user
@@ -42,18 +43,42 @@ while 1 == 1 {
         // Check the character
         switch individualCharacter {
         case "A", "B", "C", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z":
-            continue   // OK, go to next charater in the string
+        continue   // OK, go to next charater in the string
         default:
             continue  // stop looking, we"ve found bad input
         }
     }
     // If we got to this point, weknow the input is good
+    validInput = givenInput
     break  // quit the while loop and begin PROCESS section
     
 }
 
 // PROCESS
+// Now we have valid input that can be processed accroding to
+// the remaining requirements of the scenarioaccroding
 print(validInput)
+
+for letter in validInput {
+    
+    // Check each letter to make sure it's IHOSZXN
+    print(letter)
+    if letter == "I" || letter == "H" || letter == "O" || letter == "S" || letter == "X" || letter == "N" {
+        // Good - keep checking (continue loop)
+        continue
+    } else {
+        // We knwo the word doesn't work, so say NO and exit
+        print("NO")
+        exit(0)// quit the program
+    }
+    
+}
+
+// OUTPUT
+// If we got here, guaranteed this is a word with only acceptable letters
+print("YES")
+
+
 
 
 
